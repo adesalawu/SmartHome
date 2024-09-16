6b4ccc6a31c51529f4eff7210375e7dfa4f834aa
Adediran <adesalawu@outlook.com>
2024-09-16 02:42:06 +0100

    Added SmartHome script

---
+++ SmartHome.sh
@@ -0,0 +1,19 @@
+sudo apt update -y 
+sudo apt-get upgrade -y
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt-get install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+sudo docker compose up -f ./docker-compose.yaml -d
